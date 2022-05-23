7bda478cd3217ac60e79c6cf189b7ccdd3d85082
Vanina Teneva <VNTeneva19@codingburgas.bg>
2022-05-23 16:28:18 +0300

    Implement 2 libraries and 1 console app

--- lessons/20220516-BinaryFiles/20220516-BinaryFiles/20220516-BinaryFiles.cpp
+++ lessons/20220516-BinaryFiles/20220516-BinaryFiles/20220516-BinaryFiles.cpp
@@ -4,11 +4,13 @@
 
 using namespace std;
 
-#pragma pack(1)
+// #pragma pack(1)
 struct User
 {
     int id;
     bool isEnabled;
+    char name[100];
+    char* surname;
 };
 
 int main()
@@ -29,9 +31,9 @@ int main()
     int arr[] = { 65, 66, 67 };
     User u1{ 65, true };
     vector<User> users{
-        { 65, true }
-        { 66, false }
-        { 67, true }
+        { 65, true, "Ivan", new char[400] { "Ivanov"}},
+        { 66, false, "Dragan", new char[400] { "Draganov"}},
+        { 67, true, "Patkan", new char[400] { "Patkanov"}}
     };
 
     if (file.is_open())
