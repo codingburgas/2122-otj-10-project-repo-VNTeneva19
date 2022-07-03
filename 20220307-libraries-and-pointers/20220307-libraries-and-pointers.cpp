b8e9f76c13a5bed84b7fea501679ac01b2f5176a
VNTeneva19 <vnteneva19@codingburgas.bg>
2022-07-03 09:19:27 +0300

    Update file structure

--- 20220307-libraries-and-pointers/20220307-libraries-and-pointers.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "../MathUtilsLibrary/MathUtils.h"
-using namespace std;
-int main()
-{
-    cout << sum(5, 6);
-}
-
--- MathUtilsLibrary/MathUtils.cpp
+++
@@ -1,7 +0,0 @@
-#include "pch.h"
-#include "MathUtils.h"
-
-int sum(int a, int b)
-{
-    return a + b;
-}
